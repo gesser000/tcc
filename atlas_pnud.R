9928a6ad2cdf3beaf3635197fa9ce26c4bf1919f
gesser000 <gesser000@gmail.com>
2024-09-05 20:22:54 -0300

    First commit, reading data

---
+++ atlas_pnud.R
@@ -0,0 +1,15 @@
+library(readxl)
+library(dplyr)
+
+file_list <- list.files(path = "dados", pattern = "*.xlsx", full.names = TRUE)
+all_data <- lapply(file_list, read_excel) %>% 
+  bind_rows() %>%
+  filter(!(Territorialidades %in% c("Brasil", 
+                                    "Elaboração: Atlas do Desenvolvimento Humano no Brasil. Pnud Brasil, Ipea e FJP, 2022.", 
+                                    "Fontes: dados do IBGE e de registros administrativos, conforme especificados nos metadados disponíveis disponíveis em: http://atlasbrasil.org.br/acervo/biblioteca.")
+           )) %>%
+  filter(!is.na(Territorialidades))
+
+all_data <- all_data %>%
+  filter(!(Territorialidades %in% c("Brasil", "Elaboração: Atlas do Desenvolvimento Humano no Brasil. Pnud Brasil, Ipea e FJP, 2022.", "Fontes: dados do IBGE e de registros administrativos, conforme especificados nos metadados disponíveis disponíveis em: http://atlasbrasil.org.br/acervo/biblioteca.")))
+
